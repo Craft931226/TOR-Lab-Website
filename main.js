cd5c53dce08e033bbad0d25939324a844d0311c1
unknown <yanchaun0970@gmail.com>
2025-11-28 15:05:04 +0800

    20251128

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ if (navToggle && navLinks) {
       navLinks.classList.remove("is-open");
     })
   );
+
+  document.addEventListener("click", (event) => {
+    const target = event.target;
+    const clickedToggle = navToggle.contains(target);
+    const clickedMenu = navLinks.contains(target);
+    if (!clickedToggle && !clickedMenu && navLinks.classList.contains("is-open")) {
+      navLinks.classList.remove("is-open");
+    }
+  });
 }
 
 const currentPage = (() => {
